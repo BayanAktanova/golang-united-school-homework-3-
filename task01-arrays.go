275525a7f3176566a900131e6edaff455b09d62e
Баян Актанова <aktanova_b@bi.group>
2022-07-04 21:20:45 +0600

    c

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,15 +1,13 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	n, sum float32
-	
 	n := len(input)
      
     sum := 0
  
     for i := 0; i < n; i++ {
  
-        sum += float32(input[i])
+        sum += input[i]
     }
      
     avg := sum/n
